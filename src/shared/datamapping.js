41bd9240d55cc577f8d20d59e9d40c3ca26cdaa5
Mohammed Sazeed <mohammed.sazeed@bizcloudexperts.com>
2023-03-28 17:05:22 +0530

    created a lambda for Location updates as well as resources needed for it

--- src/shared/datamapping.js
+++ src/shared/datamapping.js
@@ -1,41 +1,42 @@
 
-function mapStatus(validStatusCodes) {
-    switch (validStatusCodes) {
-        case 'APL':
-            return {
-                type: 'ARRIVED',
-                StopNumber: 1
-            };
-        case 'TTC':
-            return {
-                type: 'LOADING',
-                StopNumber: 1
-            };
-        case 'COB':
-            return {
-                type: 'DEPARTED',
-                StopNumber: 1
-            };
-        case 'AAD':
-            return {
-                type: 'UNLOADING',
-                StopNumber: 2
-            };
-        case 'DEL':
-            return {
-                type: 'DELIVERED',
-                StopNumber: 2
-            };
-        case 'CAN':
-            return {
-                type: 'CANCELLED',
-                StopNumber: 2
-            };
-        default:
-            return null;
-    }
-}
 
-module.exports = {
+async function mapStatus(validStatusCodes) {
+    switch (validStatusCodes) {
+      case 'APL':
+        return {
+          type: 'ARRIVED',
+          stopNumber: 1
+        };
+      case 'TTC':
+        return {
+          type: 'LOADING',
+          stopNumber: 1
+        };
+      case 'COB':
+        return {
+          type: 'DEPARTED',
+          stopNumber: 1
+        };
+      case 'AAD':
+        return {
+          type: 'UNLOADING',
+          stopNumber: 2
+        };
+      case 'DEL':
+        return {
+          type: 'DELIVERED',
+          stopNumber: 2
+        };
+      case 'CAN':
+        return {
+          type: 'CANCELLED',
+          stopNumber: 2
+        };
+      default:
+        throw new Error(`Invalid status code: ${validStatusCodes}`);
+    }
+  }
+  
+  module.exports = {
     mapStatus,
-};
+  };  
---
+++ src/shared/dynamo.js
@@ -0,0 +1,39 @@
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+const dydb = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10" });
+
+
+async function putItem(params) {
+    try {
+
+      return await dydb.put(params).promise();
+    } catch (e) {
+      console.error("Put Item Error: ", e, "\nPut params: ", params);
+      throw "PutItemError";
+    }
+  }
+
+async function get(params) {
+    try {
+
+      return await dynamo.getItem(params).promise();
+    } catch (e) {
+      console.error("Get Item Error: ", e, "\nGet params: ", params);
+      throw "GetItemError";
+    }
+  }
+
+  async function allqueries(params) {
+    try {
+
+      return await dynamo.query(params).promise();
+    } catch (e) {
+      console.error("query Item Error: ", e, "\nquery params: ", params);
+      throw "queryItemError";
+    }
+  }
+  module.exports = {
+    get,
+    putItem,
+    allqueries
+  }
